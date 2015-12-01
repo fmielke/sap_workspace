983f7b518c59cd02dbfc8b19a96205f1c018797a
msmacco <msmacco.dev@gmail.com>
2015-12-01 16:43:45 +0100

    distancematrix api integrations

--- LocalDevelopment/BPM_BEISPIEL/htwberlin.de.tr119/bpm_beispiel_0/bl/ejb/_comp/ejbModule/demo/sap/com/services/distanz/DistanzServicetr119.java
+++ LocalDevelopment/BPM_BEISPIEL/htwberlin.de.tr119/bpm_beispiel_0/bl/ejb/_comp/ejbModule/demo/sap/com/services/distanz/DistanzServicetr119.java
@@ -1,7 +1,6 @@
 package demo.sap.com.services.distanz;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -11,34 +10,47 @@ import javax.jws.WebMethod;
 import javax.jws.WebResult;
 import javax.jws.WebService;
 
-
 @WebService
 @Stateless
 public class DistanzServicetr119 {
+
+	private final String API_KEY = "AIzaSyBngXd7HmE_nkRIojQcxJ9fGlCcc21QlNw";
 	
-	private String url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2de143494c0b295cca9337e1e96b00e0&mode=xml";
+
+	private String origins = "37081%20Goettingen%20Anna-Vandenhoeck-Ring%2050";
+	private String destinations = "10317%20Berlin%20Weitlingstrasse%20105";
+
+	
+	private String url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="+origins+"&destinations="+destinations+"=&key="+ API_KEY;
 	private String method = "GET";
 	public String jsonResponse;
-	public String sampleData;
-	
+
 	@WebMethod
-    @WebResult(name = "jsonResponse")
-	public String sendTestData() throws IOException {
+	@WebResult(name = "jsonResponse")
+	public String sendTestData() {
 
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		try {
+			URL obj = new URL(url);
 
-		// optional default is GET
-		con.setRequestMethod(method);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
+			// optional default is GET
+			con.setRequestMethod(method);
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(con
+					.getInputStream()));
+
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+			jsonResponse = response.toString();
+			return jsonResponse;
+
+		} catch (Exception e) {
+			return e.getMessage();
 		}
-		in.close();
-		jsonResponse = response.toString();
-		return jsonResponse;
 	}
 }
