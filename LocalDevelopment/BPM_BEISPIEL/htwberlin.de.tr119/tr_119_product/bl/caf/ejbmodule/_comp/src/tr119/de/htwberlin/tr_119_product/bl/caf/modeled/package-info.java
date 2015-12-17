3b7afff1cab24108c220ae66374d0c0909d29760
Fabian Mielke <1fabian.mielke@gmail.com>
2015-12-18 00:57:04 +0100

    little things and "Kundenbestaetigung Aufschlag"

--- LocalDevelopment/BPM_BEISPIEL/htwberlin.de.tr119/tr_119_product/bl/caf/ejbmodule/_comp/src/tr119/de/htwberlin/tr_119_product/bl/caf/modeled/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.sap.com/caf/htwberlin.de.tr119/tr_119_product.bl.caf/modeled")
-package tr119.de.htwberlin.tr_119_product.bl.caf.modeled;
